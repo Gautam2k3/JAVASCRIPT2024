36c39ddc8705aed4c544552edde472cccf9c7680
Gautam <112876357+Gautam2k3@users.noreply.github.com>
2024-08-05 00:13:56 +0530

    Datatyoes

---
+++ Datatype.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all code as newer version
+
+// no not required because its now already catch by node
+
+console.log(8/3);
--- index.js
+++ index.js
@@ -2,3 +2,26 @@ console.log("Hello");
 console.log(3+3);
 // console is used to print the statements.
 
+
+
+console.log("3+3");
+
+const accId = 144563;
+let accEmail = "gkusp@"
+var accPass = "12345"
+accountCity = "ALirajpur"
+
+
+// const keyword will not change
+
+let accState;
+
+console.log(accState);
+// gives you result undefined.
+
+
+
+
+
+
+
