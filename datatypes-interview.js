fbb13c8e00e923bc19af4b33ccdd382ad16e3f07
Gautam <112876357+Gautam2k3@users.noreply.github.com>
2024-08-06 21:02:54 +0530

    JUST UPDATE

---
+++ datatypes-interview.js
@@ -0,0 +1,59 @@
+// primitive and non-primitive
+
+// primitive
+
+// String
+//Number
+// Boolean
+// null
+// undefined
+// symbol
+// Bigint
+
+
+// reference type or non-primitive
+
+// Array,Objects,Function,
+
+// JS is dynamically typed language
+
+const score =100
+const scoreval = 100.45;
+
+const loogedIn = false
+
+let userEmail = undefined;
+
+const id = Symbol('123');
+const anotId = Symbol('123');
+
+
+
+console.log(id == anotId);
+//   
+
+const heroes =  ["Shaktimaan","naagraaj"]
+ 
+
+
+
+let myObj = {
+    firstName :"Gautam",
+    age :21,
+}
+
+    const myFunction = function(){
+        console.log("Hello World");
+    }
+
+
+    console.log(typeof myFunction);
+    console.log(typeof myObj);
+
+    const gameName = new String("GTA 5")
+    console.log(gameName[0]);
+    
+    
+
+    
+
