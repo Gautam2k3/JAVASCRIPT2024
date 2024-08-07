c3366f50e62519229f3cdc91079478d7825d2778
Gautam <112876357+Gautam2k3@users.noreply.github.com>
2024-08-08 00:44:16 +0530

    Commit

---
+++ date&time.js
@@ -0,0 +1,45 @@
+// Date & Time
+
+
+// jan 1 1970 se 
+
+ let myDate = new Date()
+ console.log(myDate);
+ console.log(myDate.toString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toTimeString());
+ console.log(myDate.toLocaleString());
+
+ console.log(typeof myDate);
+
+
+ let myCreatedDate = new Date(2003,0,23)
+ 
+ 
+ console.log(myCreatedDate.toDateString());
+
+ let MyDate = new Date (2003,2,2)
+
+ console.log(MyDate.toDateString())
+
+let birthDate = new Date(2005,9,5)
+console.log(birthDate.toDateString());
+
+
+let annDate = new Date("2023-01-14")
+ 
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(annDate.getTime());
+
+let newDate = newDate()
+console.log(newDate);
+
+
+
+
+
+ 
+ 
