f752e5f064395a2a3491f34166d1f878dd2e2879
Gautam <112876357+Gautam2k3@users.noreply.github.com>
2024-08-07 21:54:05 +0530

    Ek baar aur dekhna hai

---
+++ Num&math.js
@@ -0,0 +1,61 @@
+const score = 400
+
+console.log(typeof score);
+
+
+const balance = new Number(400)
+console.log(balance.toString());   //conversion from number to string 
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//    ***********Maths***********
+
+ 
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+
+console.log(Math.ceil(4.2));
+
+
+console.log(Math.min(4,3,2,1,0));
+
+
+
+console.log(Math.random());
+// 0-1
+
+const min = 10
+const max = 20
+
+     console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+// String se math and number dono ache se wapas krna
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
